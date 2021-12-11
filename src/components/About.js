ca9352066b18912a0653728d8006a9faa03a75f2
Peter K. Gorzo <peter@Peters-Air.fritz.box>
2021-12-11 17:23:08 +0100

    Added dynamic about text

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,33 @@
+import { useEffect, useState } from 'react'
+import BlockContent from '@sanity/block-content-to-react'
+import { client } from '../sanity'
+
 import css from './About.module.scss'
 
+const query = '*[_type == "about"]'
+
 export default function About(props) {
+  const [aboutText, setAboutText] = useState([])
+
+  useEffect(() => {
+    async function fetchAboutText() {
+      try {
+        const response = await client.fetch(query)
+        setAboutText(response[0].aboutText)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchAboutText()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <>
       {/* <div className={`${css.bg} ${css.revealBg}`} /> */}
       <div className={`${css.container} ${props.open && css.revealContainer}`}>
         <div className={css.textContainer}>
           <h1>Moving Forward, It Saw Moments of Beauty</h1>
-          <p>
-            {' '}
-            "Moving Forward, It Saw Moments of Beauty" (2017-ongoing) is a four
-            inch continuous watercolour painting on paper which at present spans
-            114 feet. It comes into existence in response to a life, unfolding
-            in parallel with the experience of being, and is an attempt to
-            abstract and condense this reality into the clear form of a single
-            object - a painting.{' '}
-          </p>
+          <BlockContent blocks={aboutText} />
         </div>
       </div>
     </>
