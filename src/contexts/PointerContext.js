5dc798938208cb91dd8592ba69e537a9fc26e20c
Peter K. Gorzo <peter@Peters-Air.fritz.box>
2022-01-20 00:06:04 +0100

    Temporary removal of cursor

--- src/contexts/PointerContext.js
+++ src/contexts/PointerContext.js
@@ -195,7 +195,7 @@ export function PointerProvider(props) {
         ref={translateDiv}
         className={css.pointer}
         style={{
-          opacity: suspectTouch ? 0 : 1,
+          opacity: suspectTouch ? 0 : mode === POINTER_MODE.SCROLL ? 0 : 1,
         }}
       >
         <div
